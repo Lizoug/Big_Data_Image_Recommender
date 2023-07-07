020cf1d7864309200362cb804d6e88419d065bd2
Liza Kaladjian <95773312+Lizoug@users.noreply.github.com>
2023-07-07 10:48:42 +0200

    Update main.py
    added the connection to the Database

--- Code/main.py
+++ Code/main.py
@@ -8,17 +8,20 @@ train_data_dir = "D:\images"
 batch_size = 100
 
 def main():
+    global embeddings_df, hsv_df, rgb_df
+
     # Load data
-    path_df, embeddings_df, hsv_df, rgb_df = load_dataframes()
-
-    # Update paths in DataFrames
-    path_df = update_paths_in_df(path_df, 'Path')
-
+    embeddings_df, hsv_df, rgb_df = load_dataframes()
+    
     # Run the image similarity search
-    similar_image_paths = find_all_similarities("wiese.jpg", top_n=6, type='hsv_manhattan')
-
+    similar_image_ids = find_all_similarities("test.jpg", top_n=6, type='embeddings')
+    print(similar_image_ids)
+    # Get the paths from the SQLite database
+    connection = sqlite3.connect('paths_db_3.sqlite')
+    similar_image_paths = get_paths_from_db(connection, similar_image_ids)
+    
     # Display images
-    display_images("wiese.jpg", similar_image_paths)
+    display_images("test.jpg", similar_image_paths)
 
 if __name__ == '__main__':
     main()

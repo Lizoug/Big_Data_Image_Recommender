1a4ea55373a4e376e888e7f349e803071f2fd4df
Liza Kaladjian <95773312+Lizoug@users.noreply.github.com>
2023-07-07 11:02:23 +0200

    Update plots.py

--- Code/plots.py
+++ Code/plots.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 
-def display_images(input_image_path, similar_image_paths):
+def display_input_and_similar_images(input_image_path, similar_image_paths):
     plt.figure(figsize=(20, 10))
     img = Image.open(input_image_path)
     plt.subplot(1, len(similar_image_paths) + 1, 1)
@@ -15,5 +15,17 @@ def display_images(input_image_path, similar_image_paths):
         plt.imshow(img)
         plt.title(f"Similar Image {i+1}")
         plt.axis('off')
+    plt.show()
 
+
+def display_dimension_reduction_images(similar_image_paths, similar_image_ids):
+    plt.figure(figsize=(20, 10))
+
+    for i, image_path in enumerate(similar_image_paths):
+        img = Image.open(image_path)
+        plt.subplot(1, len(similar_image_paths), i+1)
+        plt.imshow(img)
+        plt.title(f"Image ID: {similar_image_ids[i]}")  # Use the ID as the title
+        plt.axis('off')
+        
     plt.show()

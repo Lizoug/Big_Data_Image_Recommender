3ffe0331be3236cb6353aa81ca8d3226028e590e
Liza Kaladjian <95773312+Lizoug@users.noreply.github.com>
2023-07-07 10:56:59 +0200

    Update data_management.py

--- Code/data_management.py
+++ Code/data_management.py
@@ -3,11 +3,10 @@ import numpy as np
 import faiss
 
 def load_dataframes():
-    path_df = pd.read_pickle('ID_path.pkl')
-    embeddings_df = pd.read_pickle('ID_Embedings.pkl')
-    hsv_df = pd.read_pickle('ID_hsv.pkl')
-    rgb_df = pd.read_pickle('ID_rgb.pkl')
-    return path_df, embeddings_df, hsv_df, rgb_df
+    embeddings_df = pd.read_pickle('ID_Embeddings_3.pkl')
+    hsv_df = pd.read_pickle('ID_hsv_3.pkl')
+    rgb_df = pd.read_pickle('ID_rgb_3.pkl')
+    return embeddings_df, hsv_df, rgb_df
 
 def update_path(path):
     return path.replace('E:', 'D:')
@@ -16,14 +15,9 @@ def update_paths_in_df(df, path_column='Path'):
     df[path_column] = df[path_column].apply(lambda x: update_path(x))
     return df
 
-def merge_embeddings_and_paths(embeddings_df, path_df):
-    merged_df = pd.merge(embeddings_df, path_df, on='ID')
-    return merged_df
-
-def build_faiss_index(merged_df):
-    image_paths = merged_df['Path']
-    embeddings = np.array(merged_df['Embeddings'].to_list()).astype('float32')
+def build_faiss_index(embeddings_df):
+    embeddings = np.array(embeddings_df['Embeddings'].to_list()).astype('float32')
     dimension = embeddings.shape[1]
     index = faiss.IndexFlatL2(dimension)
     index.add(embeddings)
-    return index, image_paths
+    return index

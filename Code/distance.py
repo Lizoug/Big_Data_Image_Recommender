25041918bce475ab883ba8843da4d4961d25355e
Liza Kaladjian <95773312+Lizoug@users.noreply.github.com>
2023-07-07 10:54:46 +0200

    Update distance.py

--- Code/distance.py
+++ Code/distance.py
@@ -3,27 +3,16 @@ from scipy.spatial import distance
 
 
 def euclidean_distance(df, test_color, top_n):
-    if df is hsv_df:
-        df['hist_distance'] = df['HSV_Histogram'].apply(lambda x: distance.euclidean(test_color, x))
-    elif df is rgb_df:
-        df['hist_distance'] = df['RGB_Histogram'].apply(lambda x: distance.euclidean(test_color, x))
-    df.sort_values('hist_distance', inplace=True)
+    df['hist_distance'] = df['Histogram'].apply(lambda x: distance.euclidean(test_color, x))
+    df.sort_values('hist_distance', inplace=True, ascending=True)
     return df.head(top_n)
-
 
 def manhattan_distance(df, test_color, top_n):
-    if df is hsv_df:
-        df['hist_distance'] = df['HSV_Histogram'].apply(lambda x: distance.cityblock(test_color, x))
-    elif df is rgb_df:
-        df['hist_distance'] = df['RGB_Histogram'].apply(lambda x: distance.cityblock(test_color, x))
-    df.sort_values('hist_distance', inplace=True)
+    df['hist_distance'] = df['Histogram'].apply(lambda x: distance.cityblock(test_color, x))
+    df.sort_values('hist_distance', inplace=True, ascending=True)
     return df.head(top_n)
 
-
 def cosine_similarity(df, test_color, top_n):
-    if df is hsv_df:
-        df['hist_distance'] = df['HSV_Histogram'].apply(lambda x: distance.cosine(test_color, x))
-    elif df is rgb_df:
-        df['hist_distance'] = df['RGB_Histogram'].apply(lambda x: distance.cosine(test_color, x))
-    df.sort_values('hist_distance', inplace=True)
+    df['hist_similarity'] = df['Histogram'].apply(lambda x: 1 - distance.cosine(test_color, x))
+    df.sort_values('hist_similarity', inplace=True, ascending=False)
     return df.head(top_n)

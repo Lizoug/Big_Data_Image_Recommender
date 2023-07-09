f2ff6d7d2927e422db446dca3df21d256e498f2a
Liza Kaladjian <95773312+Lizoug@users.noreply.github.com>
2023-07-10 00:54:33 +0200

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -16,14 +16,17 @@ def main():
     embeddings_df, hsv_df, rgb_df = load_dataframes()
     
     # Run the image similarity search
-    similar_image_ids = find_all_similarities("test.jpg", top_n=6, type='embeddings')
+    test_image_path = "wiese.jpg"
+    similar_image_ids = find_all_similarities(test_image_path, top_n=6, type='rgb_cosine')
     print(similar_image_ids)
+    
     # Get the paths from the SQLite database
-    connection = sqlite3.connect('paths_db_3.sqlite')
+    connection = sqlite3.connect('paths_db_drive_D.sqlite')
     similar_image_paths = get_paths_from_db(connection, similar_image_ids)
     
     # Display images
-    display_input_and_similar_images("test.jpg", similar_image_paths)
+    display_input_and_similar_images(test_image_path, similar_image_paths)
+
 
 if __name__ == '__main__':
     main()
